facc6567f59462b76ef88c110e1b9e556e179091
camenduru <camenduru@gmail.com>
2024-04-29 16:11:22 +0000

    PreAuthorize

--- src/main/java/com/camenduru/web/web/rest/TypeResource.java
+++ src/main/java/com/camenduru/web/web/rest/TypeResource.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import tech.jhipster.web.util.HeaderUtil;
