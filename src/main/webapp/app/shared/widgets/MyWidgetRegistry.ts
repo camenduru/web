b142e4f22e2b7913f84f5f697a80f53137f79b6d
camenduru <camenduru@gmail.com>
2024-05-08 04:14:38 +0300

    readme widget

--- src/main/webapp/app/shared/widgets/MyWidgetRegistry.ts
+++ src/main/webapp/app/shared/widgets/MyWidgetRegistry.ts
@@ -1,9 +1,11 @@
 import { DefaultWidgetRegistry } from 'ngx-schema-form';
 import { ButtonWidget } from './button.widget';
+import { ReadmeWidget } from './readme.widget';
 
 export class MyWidgetRegistry extends DefaultWidgetRegistry {
   constructor() {
     super();
     this.register('button', ButtonWidget);
+    this.register('readme', ReadmeWidget);
   }
 }
---
+++ src/main/webapp/app/shared/widgets/readme.widget.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { Component } from '@angular/core';
+import { ControlWidget } from 'ngx-schema-form';
+
+@Component({
+  selector: 'sf-readme-widget',
+  templateUrl: 'readme.widget.html',
+})
+export class ReadmeWidget extends ControlWidget {}
