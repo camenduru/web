81446c188fe6fe06a0b958b5a51c5047618485dd
camenduru <camenduru@gmail.com>
2024-05-09 13:32:19 +0300

    *ngIf="schema.

--- src/main/webapp/app/shared/widgets/readme.widget.ts
+++ src/main/webapp/app/shared/widgets/readme.widget.ts
@@ -1,9 +1,14 @@
 /* eslint-disable */
 import { Component } from '@angular/core';
 import { ControlWidget } from 'ngx-schema-form';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'sf-readme-widget',
   templateUrl: 'readme.widget.html',
+  imports: [CommonModule],
 })
-export class ReadmeWidget extends ControlWidget {}
+export class ReadmeWidget extends ControlWidget {
+  showElement: boolean = true;
+}
