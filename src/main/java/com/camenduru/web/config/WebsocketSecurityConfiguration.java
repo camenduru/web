ff0ae0ec67ea5970000a8d99f85bf16c32499ee1
camenduru <camenduru@gmail.com>
2024-05-12 10:54:02 +0000

    test

--- src/main/java/com/camenduru/web/config/WebsocketSecurityConfiguration.java
+++ src/main/java/com/camenduru/web/config/WebsocketSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
+@SuppressWarnings("deprecation")
 @Configuration
 public class WebsocketSecurityConfiguration extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
